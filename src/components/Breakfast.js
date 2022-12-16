ad4e0eb7cf034375a7231ae457ec20085e64bc4c
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2022-12-15 22:40:48 -0500

    Add state to Breakfast.js

--- src/components/Breakfast.js
+++ src/components/Breakfast.js
@@ -1,7 +1,9 @@
-import "./Breakfast.css";
 import PropTypes from "prop-types";
+import { useState } from 'react'
 
-const Breakfast = ({name, prepTime, description, rating}) => {
+import "./Breakfast.css";
+
+const Breakfast = ({name, prepTime, description}) => {
 //   const name = props.name;
 //   const description = props.description;
 //   const prepTime = props.prepTime;
@@ -9,12 +11,35 @@ const Breakfast = ({name, prepTime, description, rating}) => {
 
 //console.log(rating);
 
+  const [rating, setRating] = useState(0);
+  // const stateArray = useState(0);
+  // const rating = stateArray[0];
+  // const setRating = stateArray[1];
+
+
+  const [breakfastName, setBreakfastName] = useState("");
+
+  const increaseRating = () => setRating(rating + 1);
+  
+  const handleClick = () => {
+    increaseRating()
+  }
+
+  const handleChange = event => setBreakfastName("eggs");
+
   return (
     <div>
       <h2>{name}</h2>
       <p>{description}</p>
       <p className="emphasizedText">Prep time: {prepTime} minutes</p>
       <p className="emphasizedText">Rating: {rating} stars</p>
+      <p>
+        Your {breakfastName} is cool!
+      </p>
+      <form>
+        <input onChange={handleChange} />
+      </form>
+      <button onClick={handleClick}>Update Rating</button>
     </div>
   );
 };
@@ -23,7 +48,6 @@ Breakfast.propTypes = {
    name:PropTypes.string.isRequired,
    description:PropTypes.string.isRequired,
    prepTime:PropTypes.number.isRequired,
-   rating:PropTypes.number.isRequired 
 }
 
 export default Breakfast;
